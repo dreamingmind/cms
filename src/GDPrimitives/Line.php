a23036463443615643ed9f4b9fe0081876b9f609
dreamingmind <ddrake@dreamingmind.com>
2022-02-24 14:59:40 -0800

    develop Line tools

--- src/GDPrimitives/Line.php
+++ src/GDPrimitives/Line.php
@@ -6,23 +6,22 @@ namespace App\GDPrimitives;
 class Line
 {
 
-    public function __construct(Point $pt1, Point $pt2, $color, $canvas)
+    public function __construct(Point $pt1, Point $pt2, Color $color)
     {
         $this->pt1 = $pt1;
         $this->pt2 = $pt2;
         $this->color = $color;
-        $this->canvas = $canvas;
     }
 
-    public function add()
+    public function add($canvas)
     {
         imageline(
-            $this->canvas,
+            $canvas,
             $this->pt1->x(),
             $this->pt1->y(),
             $this->pt2->x(),
             $this->pt2->y(),
-            $this->color
+            $this->color->allocate($canvas)
         );
 
     }
