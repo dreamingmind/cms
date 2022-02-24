11d00dbad4f0974b3c501cc32cfaa16e42c9db3b
dreamingmind <ddrake@dreamingmind.com>
2022-02-23 22:05:41 -0800

    Build a Color class

---
+++ src/GDPrimitives/Color.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\GDPrimitives;
+
+class Color
+{
+       protected $r = 255;
+       protected $g = 255;
+       protected $b = 255;
+
+    public function setColor($r, $g, $b): Color
+    {
+        $this->r = $r;
+        $this->g = $g;
+        $this->b = $b;
+        return $this;
+    }
+
+    public function grey($percent): Color
+    {
+        $val = (int) (255 - (255 * ($percent / 100)));
+        return $this->setColor($val, $val, $val);
+    }
+
+    public function allocate($image)
+    {
+        return imagecolorallocate($image, $this->r, $this->g, $this->b);
+    }
+
+}
