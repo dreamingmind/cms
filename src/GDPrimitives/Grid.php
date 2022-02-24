465488ca278a5c398e1993a152a2a180faeb4241
dreamingmind <ddrake@dreamingmind.com>
2022-02-24 14:58:32 -0800

    develop grid tools

--- src/GDPrimitives/Grid.php
+++ src/GDPrimitives/Grid.php
@@ -36,10 +36,52 @@ class Grid
         );
     }
 
+    public function color() : Color
+    {
+        return $this->getConfig('grid_color');
+    }
+
     public function xLine($x)
     {
-        $p1 = new Point($this->tile * $x + $this->pt1->x(), $this->pt1->y());
-        $p2 = new Point($this->tile * $x + $this->pt1->x(), $this->pt2->y());
-        return new PointPair($p1, $p2);
+        $p1 = new Point(
+            $this->getX($x) + $this->region->originX(),
+            $this->region->y(0) + $this->region->originY()
+        );
+        $p2 = new Point(
+            $this->getX($x) + $this->region->originX(),
+            $this->region->y(100) + $this->region->originY()
+        );
+        return new Line($p1, $p2, $this->color());
+    }
+
+    public function yLine($y)
+    {
+        $p1 = new Point(
+            $this->region->x(0) + $this->region->originX(),
+            $this->getY($y) + $this->region->originY()
+        );
+        $p2 = new Point(
+            $this->region->x(100) + $this->region->originX(),
+            $this->getY($y) + $this->region->originY()
+        );
+        return new Line($p1, $p2, $this->color());
+    }
+
+    /**
+     * @param int $n grid increment
+     * @return float|int
+     */
+    private function getX($n)
+    {
+        return $n * $this->region->tileSize();
+    }
+
+    /**
+     * @param int $n grid increment
+     * @return float|int
+     */
+    private function getY($n)
+    {
+        return $n * $this->region->tileSize();
     }
 }
