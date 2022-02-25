f05a73c13772e4e76838a4a24ed3bc53039312b4
dreamingmind <ddrake@dreamingmind.com>
2022-02-25 12:43:19 -0800

    stub in new class function
    flyweight rectangle can do variable width strokes now

--- src/GDPrimitives/FlyweightRectangle.php
+++ src/GDPrimitives/FlyweightRectangle.php
@@ -5,24 +5,37 @@ namespace App\GDPrimitives;
 class FlyweightRectangle
 {
 
-    public function stroke($canvas, $tile, $width)
+    /**
+     * @param $canvas
+     * @param Point[] $points
+     * @param $width
+     * @return void
+     */
+    public function stroke($canvas, array $points, $width)
     {
-        $this->$tile = $tile;
+
         foreach (range(0, $width - 1) as $count) {
-            $this->perimeter($count);
+            $this->perimeter($canvas, $points, $count);
         }
     }
 
-    private function perimeter($count)
+    private function perimeter($canvas, $points, $count)
     {
+        /* @var Point $p1 */
+        /* @var Point $p2 */
+
+        $p1 = $points[0];
+        $p2 = $points[1];
+        $c = (new Color())->grey(100);
+
         imagerectangle(
-            $this->_canvas,
-            $this->tile->xLo() + $count,
-            $this->tile->yLo() + $count,
-            $this->tile->xHi() - $count,
-            $this->tile->yHi() - $count,
+            $canvas,
+            $p1->x() + $count,
+            $p1->y() + $count,
+            $p2->x() - $count,
+            $p2->y() - $count,
 //            (new Color())->setColor(90,200, 55)->allocate($this->_canvas)
-            (new Color())->grey(100)->allocate($this->_canvas)
+            $c->allocate($canvas)
         );
 
     }
