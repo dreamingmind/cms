5bef34fbb6575c84ba92ccc55e52c50d3c20c8a1
dreamingmind <ddrake@dreamingmind.com>
2022-02-25 11:04:14 -0800

    revise Tile class
    work out a shorter, but more expressive interface

--- src/GDPrimitives/Tile.php
+++ src/GDPrimitives/Tile.php
@@ -2,6 +2,8 @@
 
 namespace App\GDPrimitives;
 
+use phpDocumentor\Reflection\PseudoTypes\NumericString;
+
 class Tile
 {
 
@@ -12,22 +14,52 @@ class Tile
         $this->xHi = $grid->getX($xIndex) - 1;
         $this->yLo = $grid->getY($yIndex - 1) +1;
         $this->yHi = $grid->getY($yIndex) - 1;
+        $this->x = [
+            'hi' => $grid->getX($xIndex) - 1,
+            'lo' => $grid->getX($xIndex - 1) +1
+            ];
+        $this->y = [
+            'hi' => $grid->getY($yIndex) - 1,
+            'lo' => $grid->getY($yIndex - 1) +1,
+            ];
     }
 
-    public function xLo()
+    /**
+     * @param $i 'lo', 'hi', int representing a % between lo & hi
+     * @return int
+     */
+    public function getX($i)
     {
-        return $this->xLo;
+        if(is_numeric($i)) {
+            return $this->percent($this->getX('lo'), $this->getX('hi'), $i);
+        }
+        return $this->x[$i];
     }
-    public function xHi()
+
+    /**
+     * @param $i 'lo', 'hi', int representing a % between lo & hi
+     * @return int
+     */
+    public function getY($i)
     {
-        return $this->xHi;
+        if(is_numeric($i)) {
+            return $this->percent($this->getY('lo'), $this->getY('hi'), $i);
+        }
+        return $this->y[$i];
     }
-    public function yLo()
+
+    public function getPoint($xPC, $yPC)
     {
-        return $this->yLo;
+        return new Point($this->getX($xPC), $this->getY($yPC));
     }
-    public function yHi()
+
+    private function loHi($i)
     {
-        return $this->yHi;
+        return in_array($i, ['lo', 'hi']);
+    }
+
+    private function percent(int $lo, int $hi, int $pc)
+    {
+        return (int) (($hi - $lo) * ($pc / 100)) + $lo;
     }
 }
