2bfe7145e864ec0b1c4dc472bd08d99c3a390f1c
dreamingmind <ddrake@dreamingmind.com>
2022-03-01 11:12:06 -0800

    test files

---
+++ tests/TestCase/Controller/DungeonsControllerTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Test\TestCase\Controller;
+
+class DungeonsControllerTest extends \Cake\TestSuite\TestCase
+{
+
+
+
+}
---
+++ tests/TestCase/Lib/RegionTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Test\TestCase\Lib;
+
+use App\GDPrimitives\Canvas;
+
+class RegionTest extends \Cake\TestSuite\TestCase
+{
+
+    public function test_Render()
+    {
+        $c = new Canvas();
+        $x = 'thing';
+    }
+}
