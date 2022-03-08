a7bd7f06b29cf01761856fa4098c4c87892e8dc8
dreamingmind <ddrake@dreamingmind.com>
2022-03-07 22:58:21 -0800

    build up Region tests

--- tests/TestCase/Lib/RegionTest.php
+++ tests/TestCase/Lib/RegionTest.php
@@ -4,6 +4,7 @@ namespace App\Test\TestCase\Lib;
 
 use App\Constants\Con;
 use App\GDPrimitives\Color;
+use App\GDPrimitives\PointPair;
 use App\Lib\Canvas;
 use App\Lib\Region;
 
@@ -13,8 +14,6 @@ class RegionTest extends \Cake\TestSuite\TestCase
     public function test_configUsedToMakeCanvas()
     {
         $config = [
-            'origin_x' => 10,
-            'origin_y' => 10,
             'tiles_wide' => 10,
             'tiles_high' => 10,
             'tile_size' => 10,
@@ -26,33 +25,109 @@ class RegionTest extends \Cake\TestSuite\TestCase
         $this->assertTrue($canvas == $region->canvas());
     }
 
-    public function test_RegionMethodsDelgateToCanvas()
+    public function test_Image_DelgatesToCanvas()
     {
-        /**
-         * tileSize
-         * origin
-         * width
-         * height
-         * image
-         */
         $canvas = $this->createMock(Canvas::class);
-        $canvas->expects($this->once())
-            ->method('tileSize');
-        $canvas->expects($this->once())
-            ->method('origin');
-        $canvas->expects($this->once())
-            ->method('width');
-        $canvas->expects($this->once())
-            ->method('height');
         $canvas->expects($this->once())
             ->method('image');
 
         $region = new Region(['canvas' => $canvas]);
-        $region->tileSize();
-        $region->origin(Con::X);
-        $region->width(Con::TILE);
-        $region->height(Con::PIXEL);
         $region->image();
-        debug($region->canvas());
     }
+
+    public function test_Height_DelgatesToCanvas()
+    {
+        $canvas = $this->createMock(Canvas::class);
+        $canvas->expects($this->atLeastOnce())
+            ->method('height');
+
+        $region = new Region(['canvas' => $canvas]);
+        $region->height(Con::PIXEL);
+    }
+
+    public function test_Width_DelgatesToCanvas()
+    {
+        $canvas = $this->createMock(Canvas::class);
+        $canvas->expects($this->atLeastOnce())
+            ->method('width');
+
+        $region = new Region(['canvas' => $canvas]);
+        $region->width(Con::TILE);
+    }
+
+    public function test_Origin_DelgatesToCanvas()
+    {
+        $canvas = $this->createMock(Canvas::class);
+        $canvas->expects($this->atLeastOnce())
+            ->method('origin');
+
+        $region = new Region(['canvas' => $canvas]);
+        $region->origin(Con::X);
+    }
+
+    public function test_Tile_SizeDelgatesToCanvas()
+    {
+        $canvas = $this->createMock(Canvas::class);
+        $canvas->expects($this->once())
+            ->method('tileSize');
+
+        $region = new Region(['canvas' => $canvas]);
+        $region->tileSize();
+    }
+
+    public function test_points()
+    {
+        $region = new Region();
+
+        $this->assertInstanceOf(PointPair::class, $region->points());
+    }
+
+    public function test_GetColorReadsCanvasColor()
+    {
+        $config = [
+            'ground_color' => (new Color('test'))->setColor(0,0,0)
+        ];
+        $region = new Region($config);
+
+        //read canvas directly
+        $this->assertEquals(
+            [0,0,0],
+            array_values(
+                $region->canvas()->_getColor('current')->getColorValue()
+            )
+        );
+
+        //read through Region
+        $this->assertEquals(
+            [0,0,0],
+            array_values(
+                $region->getColor('current')->getColorValue()
+            )
+        );
+    }
+
+    public function test_SetColorSetsCanvasColor()
+    {
+        $config = [
+            'ground_color' => (new Color('test', [0,0,0]))
+        ];
+        $region = new Region($config);
+        $region->setColor('new', [127,127,127]);
+
+        $this->assertEquals(
+            [127,127,127],
+            array_values(
+                $region->getColor('current')->getColorValue()
+            )
+        );
+    }
+
+    public function test_SetColorIsChainable()
+    {
+        $region = new Region();
+        $actual = $region->setColor('new', [127,127,127]);
+
+        $this->assertInstanceOf(Region::class, $actual);
+    }
+
 }
