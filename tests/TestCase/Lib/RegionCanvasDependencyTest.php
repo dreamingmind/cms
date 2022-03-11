d0598a5befd826c6fa44aa4fd59c623757a48667
dreamingmind <ddrake@dreamingmind.com>
2022-03-11 12:19:41 -0800

    fix typos

--- tests/TestCase/Lib/RegionCanvasDependencyTest.php
+++ tests/TestCase/Lib/RegionCanvasDependencyTest.php
@@ -6,6 +6,7 @@ use App\Constants\Con;
 use App\GDPrimitives\Color;
 use App\Lib\Canvas;
 use App\Lib\Region;
+use App\Traits\ColorRegistryTrait;
 
 class RegionCanvasDependencyTest extends \Cake\TestSuite\TestCase
 {
@@ -24,7 +25,7 @@ class RegionCanvasDependencyTest extends \Cake\TestSuite\TestCase
         $this->assertTrue($canvas == $region->canvas());
     }
 
-    public function test_Image_DelgatesToCanvas()
+    public function test_Image_DelegatesToCanvas()
     {
         $canvas = $this->createMock(Canvas::class);
         $canvas->expects($this->once())
@@ -34,7 +35,7 @@ class RegionCanvasDependencyTest extends \Cake\TestSuite\TestCase
         $region->image();
     }
 
-    public function test_Height_DelgatesToCanvas()
+    public function test_Height_DelegatesToCanvas()
     {
         $canvas = $this->createMock(Canvas::class);
         $canvas->expects($this->atLeastOnce())
@@ -44,7 +45,7 @@ class RegionCanvasDependencyTest extends \Cake\TestSuite\TestCase
         $region->height(Con::PIXEL);
     }
 
-    public function test_Width_DelgatesToCanvas()
+    public function test_Width_DelegatesToCanvas()
     {
         $canvas = $this->createMock(Canvas::class);
         $canvas->expects($this->atLeastOnce())
@@ -54,7 +55,7 @@ class RegionCanvasDependencyTest extends \Cake\TestSuite\TestCase
         $region->width(Con::TILE);
     }
 
-    public function test_Origin_DelgatesToCanvas()
+    public function test_Origin_DelegatesToCanvas()
     {
         $canvas = $this->createMock(Canvas::class);
         $canvas->expects($this->atLeastOnce())
@@ -64,7 +65,7 @@ class RegionCanvasDependencyTest extends \Cake\TestSuite\TestCase
         $region->origin(Con::X);
     }
 
-    public function test_Tile_SizeDelgatesToCanvas()
+    public function test_Tile_SizeDelegatesToCanvas()
     {
         $canvas = $this->createMock(Canvas::class);
         $canvas->expects($this->once())
