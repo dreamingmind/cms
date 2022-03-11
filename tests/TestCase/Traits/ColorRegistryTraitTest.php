5714be6a4ed889351b53e0507e62f15539c9d31f
dreamingmind <ddrake@dreamingmind.com>
2022-03-10 16:17:22 -0800

    expand tests

--- tests/TestCase/Traits/ColorRegistryTraitTest.php
+++ tests/TestCase/Traits/ColorRegistryTraitTest.php
@@ -5,14 +5,20 @@ namespace App\Test\TestCase\Traits;
 use App\GDPrimitives\Color;
 use App\Lib\ColorRegistry;
 use App\Traits\ColorRegistryTrait;
+use Cake\TestSuite\TestCase;
 
-class ColorRegistryTraitTest extends \Cake\TestSuite\TestCase
+class ColorRegistryTraitTest extends TestCase
 {
 
+    /**
+     * Clear the Static Registry Class each time
+     */
     public function setUp() : void
     {
         ColorRegistry::reset();
     }
+
+    //<editor-fold desc="Tests for Registry::_getColor() functionality">
 
     public function test__getColorReturnsFullRegistrySet()
     {
@@ -44,13 +50,30 @@ class ColorRegistryTraitTest extends \Cake\TestSuite\TestCase
         $this->assertCount(1, $registryEntries);
         $this->assertArrayHasKey('rando', $registryEntries);
     }
+    //</editor-fold>
 
+    public function test_getColorWhenNoneDefined()
+    {
+        $class = new UserOfColorTrait();
 
+        $this->assertEmpty($class->getColor());
+        $this->assertNull($class->getColor('white'));
+    }
 
     private function makeBlackAndWhite($class)
     {
         $class->_setColor('black', [0,0,0]);
         $class->_setColor('white', [255,255,255]);
+    }
+
+    private function makeWhiteFillColor($class)
+    {
+        $class->setColor('fill', 'white', [255,255,255]);
+    }
+
+    private function makeBlackStrokeColor($class)
+    {
+        $class->setColor('stroke', 'black', [0,0,0]);
     }
 }
 
